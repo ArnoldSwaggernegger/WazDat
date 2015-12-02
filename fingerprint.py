8c1e3892773b7fca793af8d10c443d7a66ccc9aa
Mathijs <mathijs.l.molenaar@gmail.com>
2015-12-02 15:43:35 +0100

    Dont process last (incomplete) window and added function to show spectogram <3 pyplot

--- fingerprint.py
+++ fingerprint.py
@@ -10,10 +10,9 @@ def get_fingerprints(signal):
 def get_spectogram(signal, window_size):
     result = []
     samples = signal.get_samples()
-    for i in range(0, len(samples), window_size):
-        r = min(len(samples), i + window_size)
-        window = zero_padded_window(r - i)
-        partial_signal = window * samples[i:r]
+    for i in range(0, len(samples)-window_size, window_size):
+        window = zero_padded_window(window_size)
+        partial_signal = window * samples[i:i+window_size]
         spectrum = np.fft.rfft(partial_signal)
         frequencies = np.fft.rfftfreq(
             len(partial_signal), 1.0 / signal.get_samplerate()
@@ -44,11 +43,29 @@ def hamming_window(length, index, size):
     '''
     return None
 
+def show_spectogram(spectogram, time):
+    frequencies0, _ = spectogram[0]
+    width = len(spectogram)
+    height = len(frequencies0)
+    print width, height
+
+    image = np.empty((width, height))
+    for x in xrange(width):
+        _, spectrum = spectogram[x]
+        image[x] = spectrum
+
+    plt.imshow(image.T, cmap=plt.cm.Reds, aspect="auto", extent=[0, time, frequencies0[0], frequencies0[-1]])
+    ax = plt.gca()
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Frequencies")
+    plt.show()
+
 if __name__ == "__main__":
     import soundfiles
     signal = soundfiles.load_wav("test/muziek.wav")
     spectogram = get_spectogram(signal, 800)
-    for sample in spectogram[:3]:
+    """for sample in spectogram[:3]:
         (freqs, spectrum) = sample
         plt.plot(freqs, spectrum)
-        plt.show()
+        plt.show()"""
+    show_spectogram(spectogram, 1.0 * len(signal.get_samples()) / signal.get_samplerate())
