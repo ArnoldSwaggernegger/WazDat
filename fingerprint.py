a547bedf81e6d19929deb339fa13776cf8eff127
Scoudem <tvvaalen@gmail.com>
2015-12-02 12:24:53 +0100

    Skeleton of fingerprint

--- fingerprint.py
+++ fingerprint.py
@@ -1,5 +1,35 @@
-def get_fingerprints(signal):
-    
-    # get spectogram peaks as (time, frequency) points
-    
+import numpy as np
+
+
+def get_fingerprints(signal, window_size=100):
+    '''
+    get spectrogram peaks as (time, frequency) points
+    '''
+
+    samples = signal.get_samples()
+
+    for i in range(0, samples.length, window_size):
+        window = zero_padded_window(len(samples), i, window_size)
+        partial_signal = window * samples
+        spectrum = np.fft.fft(partial_signal)
+        frequencies = np.fft.fftfreq(len(spectrum))
+
+
+def zero_padded_window(length, index, size):
+    '''
+    '''
+    window = np.zeros(length)
+    window[index:index + size] = 1
+    return window
+
+
+def hanning_window(length, index, size):
+    '''
+    '''
+    return None
+
+
+def hamming_window(length, index, size):
+    '''
+    '''
     return None
