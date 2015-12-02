ecfa038454ec0d934ce010f287f77d76dd645e39
Scoudem <tvvaalen@gmail.com>
2015-12-02 12:44:46 +0100

    spectrum

--- fingerprint.py
+++ fingerprint.py
@@ -1,26 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def get_fingerprints(signal, window_size=100):
+def get_fingerprints(signal, window_size=8000):
     '''
     get spectrogram peaks as (time, frequency) points
     '''
 
     samples = signal.get_samples()
 
-    for i in range(0, samples.length, window_size):
-        window = zero_padded_window(len(samples), i, window_size)
-        partial_signal = window * samples
-        spectrum = np.fft.fft(partial_signal)
-        frequencies = np.fft.fftfreq(len(spectrum))
+    for i in range(0, len(samples), window_size):
+        window = zero_padded_window(window_size)
+        partial_signal = window * samples[i:i + window_size]
+        spectrum = np.fft.rfft(partial_signal)
+        frequencies = np.fft.rfftfreq(
+            len(partial_signal), 1.0 / signal.get_samplerate()
+        )
+
+        # plt.plot(frequencies, np.abs(spectrum))
+        # plt.show()
 
 
-def zero_padded_window(length, index, size):
+def zero_padded_window(size):
     '''
     '''
-    window = np.zeros(length)
-    window[index:index + size] = 1
-    return window
+    return np.ones(size)
 
 
 def hanning_window(length, index, size):
