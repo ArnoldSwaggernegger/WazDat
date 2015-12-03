04c5c26e81e2bf0f261f04b9c847a294bbc7fa0b
Mathijs <mathijs.l.molenaar@gmail.com>
2015-12-03 12:50:50 +0100

    Basic binning in get_spectogram

--- fingerprint.py
+++ fingerprint.py
@@ -7,17 +7,22 @@ def get_fingerprints(signal):
     get spectrogram peaks as (time, frequency) points
     '''
 
-def get_spectogram(signal, window_size):
+def get_spectogram(signal, window_size, bin_size):
     samples = signal.get_samples()
     width = len(samples) / window_size
-    height = window_size / 2
+    height = window_size / 2 / bin_size
     window = zero_padded_window(window_size)
 
     result = np.empty((width, height))
     for t in range(0, width):
         partial_signal = window * samples[t*window_size:(t+1)*window_size]
-        spectrum = np.fft.rfft(partial_signal)[:height]
-        result[t] = np.abs(spectrum)
+        spectrum = np.fft.rfft(partial_signal)
+
+        for bin in xrange(height):
+            sum = 0.0
+            for offset in xrange(bin_size):
+                sum += np.abs(spectrum[bin*bin_size+offset])
+            result[t][bin] = sum
 
     return result
 
@@ -54,7 +59,7 @@ def show_spectogram(spectogram, time):
 if __name__ == "__main__":
     import soundfiles
     signal = soundfiles.load_wav("audio/gaai.wav")
-    spectogram = get_spectogram(signal, 1024)
+    spectogram = get_spectogram(signal, 2048, 2)
     """for sample in spectogram[:3]:
         (freqs, spectrum) = sample
         plt.plot(freqs, spectrum)
