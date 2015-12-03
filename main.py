8a3b39a6f0390974aac2da7eebd6a9784096efee
Matthijs de Wit <matthijs111@gmail.com>
2015-12-03 14:42:21 +0100

    beginning of the main

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import soundfiles
 import fingerprint
+import sys
 
 if __name__ == "__main__":
 
-    signal = soundfiles.load_wav("test/muziek.wav")
-    fingerprint.get_fingerprints(signal)
+    tutorial = "use: main.py [database] [signal]"
+
+    try:
+        database = database.load(arg[1])
+    except:
+        print "something went wrong with loading the database\n", tutorial
+        sys.exit()
+
+    try:
+        signal = soundfiles.load_signal("audio/" + arg[2])
+        if signal == None:
+            raise
+    except:
+         print "something went wrong with loading the signal\n", tutorial
+         sys.exit()
+
+    # fingerprint.get_fingerprints(signal)
 
     # create classifier from database
-
-    # get arguments
 
     # get input file fingerprints
 
