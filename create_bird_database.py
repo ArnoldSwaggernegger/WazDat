21b21ae2734332f5cda4f9371e6eb2d9447f2c15
Matthijs de Wit <matthijs111@gmail.com>
2015-12-08 12:46:40 +0100

    made a new class Create_Database

--- create_bird_database.py
+++ create_bird_database.py
@@ -1,4 +1,6 @@
+from sys import argv, exit
 from os import remove, path
+from glob import glob
 from database import Database
 from soundfiles import find_files, load_signal
 from fingerprint import get_tokens
@@ -7,20 +9,55 @@ from fingerprint import get_tokens
 # make this program generic:
 # requires: database-filename, file expression 
 # the second might be hard as terminals automatically expand *
+def remove_old(name):
+    if path.isfile("/databases" + name):
+        remove("/databases" + name)
 
-if __name__ == "__main__":
-    if path.isfile(Database.DBPREFIX + "vogels"):
-        remove(Database.DBPREFIX + "vogels") 
+def find_wav_files(directory):
+    return glob(directory + "*.wav")
 
-    database = Database("vogels")
+class Create_Database:
+    """
+    define name, training set directory and database object of the new database 
+    """
+    def __init__(self, name, directory):
+        self.name = name
+        self.directory = directory
+        self.database = Database(name)
     
-    for filename in ["training/track01_ijsvogel.wav", "training/track03_goudvink.wav", "training/track09_roerdomp.wav"]: #find_files("training/*.wav"):
-        with open(filename, "r") as f:
-            print "Reading {}".format(filename)
-            signal = load_signal(filename)
-            print "Analyzing {}".format(filename)
-            database.add(get_tokens(signal))
-            print "Done"
+    """
+    create a new database from 
+    """
+    def new_database(self):
+        remove_old(self.name)
+
+        audio_files = find_wav_files(self.directory)
+
+        for filename in audio_files:
+            with open(filename, "r") as f:
+                print "Reading {}".format(filename)
+                signal = load_signal(filename)
+                print "Analyzing {}".format(filename)
+                self.database.add(get_tokens(signal))
+                print "Done"
+
+        print "Writing database to disk"
+        self.database.save()
+
+        
+if __name__ == "__main__":
+
+    if len(argv) < 3:
+        print "usage: create_bird_database [database name] [training directory]"
+        exit()
+
+    name = argv[1]
+    directory = argv[2]
+
+    new_db = Create_Database(name, directory)
+
+    new_db.new_database()
+
+    
             
-    print "Writing database to disk"
-    database.save()  
+      
