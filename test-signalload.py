29eebd668763b63ed642ab3ec6c2f347738db907
Matthijs de Wit <matthijs111@gmail.com>
2015-12-09 11:45:59 +0100

    new audio files and seperate test script

---
+++ test-signalload.py
@@ -0,0 +1,104 @@
+import soundfiles
+import fingerprint
+import numpy as np
+import matplotlib.pyplot as plt
+
+signal = soundfiles.load_wav("training/pokemon/003.wav")
+
+x = []
+for i in signal:
+    x.append(i)
+
+plt.subplot(411)
+plt.title("original")
+plt.plot(x)
+
+'''
+fingers =  get_fingerprints(signal, 2048, 8)
+time = []
+peaks = []
+
+for t, ps in fingers:
+    time  += [t] * len(ps)
+    peaks += ps
+
+plt.subplot(221)
+plt.title("original")
+plt.scatter(time, peaks, color="red")
+'''
+
+signal = soundfiles.load_wav("audio/pokemon/003-divided.wav")
+
+x = []
+for i in signal:
+    x.append(i)
+
+plt.subplot(412)
+plt.title("divided")
+plt.plot(x)
+
+'''
+fingers =  get_fingerprints(signal, 2048, 8)
+time = []
+peaks = []
+
+for t, ps in fingers:
+    time  += [t] * len(ps)
+    peaks += ps
+
+plt.subplot(222)
+plt.title("amplified")
+plt.scatter(time, peaks, color="red")
+'''
+
+signal = soundfiles.load_wav("audio/pokemon/003-translated.wav")
+
+x = []
+for i in signal:
+    x.append(i)
+
+plt.subplot(413)
+plt.title("offset")
+plt.xlim(20480,40480)
+plt.plot(x)
+
+'''
+fingers =  get_fingerprints(signal, 2048, 8)
+time = []
+peaks = []
+
+for t, ps in fingers:
+    time  += [t] * len(ps)
+    peaks += ps
+
+plt.subplot(223)
+plt.title("offset")
+plt.scatter(time, peaks, color="red")
+'''
+
+signal = soundfiles.load_wav("audio/pokemon/003-noise.wav")
+
+x = []
+for i in signal:
+    x.append(i)
+
+plt.subplot(414)
+plt.title("noise")
+plt.plot(x)
+plt.show()
+
+'''
+fingers =  get_fingerprints(signal, 2048, 8)
+time = []
+peaks = []
+
+for t, ps in fingers:
+    time  += [t] * len(ps)
+    peaks += ps
+
+
+plt.subplot(224)
+plt.title("noise")
+plt.scatter(time, peaks, color="red")
+plt.show()
+'''
